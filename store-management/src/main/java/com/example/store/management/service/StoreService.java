b6c6b7a91328ef6c89ba5c92be851ade98dd28f8
corina <valeriabalan1997@gmail.com>
2024-09-21 16:19:26 +0300

    saving store in service layer

--- store-management/src/main/java/com/example/store/management/service/StoreService.java
+++ store-management/src/main/java/com/example/store/management/service/StoreService.java
@@ -1,4 +1,38 @@
 package com.example.store.management.service;
 
+
+import com.example.store.management.dto.StoreDto;
+import com.example.store.management.entity.Product;
+import com.example.store.management.entity.Store;
+import com.example.store.management.mapper.ProductMapper;
+import com.example.store.management.mapper.StoreMapper;
+import com.example.store.management.repository.ProductRepository;
+import com.example.store.management.repository.StoreRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class StoreService {
+
+    private final StoreRepository storeRepository;
+    private final StoreMapper storeMapper;
+    private final ProductRepository productRepository;
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+
+    @Transactional
+    public StoreDto saveStore(StoreDto storeDto){
+        Store store=storeMapper.convert(storeDto);
+        //saving store first
+        storeRepository.save(store);
+
+        //saving products
+        for(Product product:store.getProduct()){
+            product.setStore(store);
+            productRepository.save(product);
+        }
+        return storeMapper.convert(store);
+    }
 }
