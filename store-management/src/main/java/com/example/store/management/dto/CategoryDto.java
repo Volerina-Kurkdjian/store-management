4c8d5a118c0ba984e1c3cf5b1d187dbcaee0c160
corina <valeriabalan1997@gmail.com>
2024-09-21 15:14:48 +0300

    adding fields to CategoryDto class

--- store-management/src/main/java/com/example/store/management/dto/CategoryDto.java
+++ store-management/src/main/java/com/example/store/management/dto/CategoryDto.java
@@ -1,4 +1,16 @@
 package com.example.store.management.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class CategoryDto {
+
+    @JsonProperty(access=JsonProperty.Access.READ_ONLY)
+    private String categoryId;
+    private String name;
+    List<ProductDto> product=new ArrayList<>();
 }
