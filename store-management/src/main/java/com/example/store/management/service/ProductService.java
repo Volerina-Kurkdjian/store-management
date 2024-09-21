8a0bf620eae0a3dc9d5c3bd5c61c6f570a8f1dd7
corina <valeriabalan1997@gmail.com>
2024-09-21 15:38:20 +0300

    implement method create product in ProductService

--- store-management/src/main/java/com/example/store/management/service/ProductService.java
+++ store-management/src/main/java/com/example/store/management/service/ProductService.java
@@ -1,6 +1,13 @@
 package com.example.store.management.service;
 
+import com.example.store.management.dto.CategoryDto;
+import com.example.store.management.dto.ProductDto;
+import com.example.store.management.entity.Category;
+import com.example.store.management.mapper.CategoryMapper;
+import com.example.store.management.mapper.ProductMapper;
+import com.example.store.management.repository.CategoryRepository;
 import com.example.store.management.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +16,22 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
+    @Transactional
+    public ProductDto createProduct(ProductDto productDto){
+        if(productDto.getCategory()!=null){
+            Category category=categoryMapper.convert(productDto.getCategory());
+            //set child to parent first
+            category.getProduct().add(productMapper.convert(productDto));
+            //save parent
+            categoryRepository.save(category);
+        }
+        //save child in db regardless
+        return productMapper.convert(productRepository.save(productMapper.convert(productDto)));
+    }
 
 
 }
