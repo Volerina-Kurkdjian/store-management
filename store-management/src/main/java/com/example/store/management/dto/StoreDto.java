8546fc2cc83beeedaa328584814ac5a0a40aa695
corina <valeriabalan1997@gmail.com>
2024-09-21 15:19:49 +0300

    adding fields to StoreDto class

--- store-management/src/main/java/com/example/store/management/dto/StoreDto.java
+++ store-management/src/main/java/com/example/store/management/dto/StoreDto.java
@@ -1,4 +1,16 @@
 package com.example.store.management.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class StoreDto {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String storeId;
+    private String name;
+    private String contactInfo;
+    List<ProductDto> product=new ArrayList<>();
 }
