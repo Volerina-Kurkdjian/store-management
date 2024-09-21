0f59c05c3ba8e4d339655940544fc4a22e16fab3
corina <valeriabalan1997@gmail.com>
2024-09-21 15:17:07 +0300

    adding fields to ProductDto class

--- store-management/src/main/java/com/example/store/management/dto/ProductDto.java
+++ store-management/src/main/java/com/example/store/management/dto/ProductDto.java
@@ -1,4 +1,19 @@
 package com.example.store.management.dto;
 
+import com.example.store.management.entity.Category;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
 public class ProductDto {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String productId;
+    private String name;
+    private String description;
+    private Long price;
+
+    @JsonIgnore
+    private Category category;
 }
