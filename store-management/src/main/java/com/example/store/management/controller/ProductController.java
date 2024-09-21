cebb4e9f18b77cb54e7ad0753b87ceecbce7ef32
corina <valeriabalan1997@gmail.com>
2024-09-21 15:48:24 +0300

    implementing save product method in controller

--- store-management/src/main/java/com/example/store/management/controller/ProductController.java
+++ store-management/src/main/java/com/example/store/management/controller/ProductController.java
@@ -1,4 +1,24 @@
 package com.example.store.management.controller;
 
+import com.example.store.management.dto.ProductDto;
+import com.example.store.management.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto){
+        return ResponseEntity.status(HttpStatus.OK).body(productService.createProduct(productDto));
+    }
 }
