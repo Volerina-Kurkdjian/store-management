09eb3aa1a5ff2a04d7f39732e1cd4e3a0177d053
corina <valeriabalan1997@gmail.com>
2024-09-21 16:59:07 +0300

    creating category functionality

--- store-management/src/main/java/com/example/store/management/controller/CategoryController.java
+++ store-management/src/main/java/com/example/store/management/controller/CategoryController.java
@@ -1,4 +1,24 @@
 package com.example.store.management.controller;
 
+import com.example.store.management.dto.CategoryDto;
+import com.example.store.management.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/categories")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("/create")
+    public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.saveCategory(categoryDto));
+    }
 }
--- store-management/src/main/java/com/example/store/management/service/CategoryService.java
+++ store-management/src/main/java/com/example/store/management/service/CategoryService.java
@@ -1,4 +1,36 @@
 package com.example.store.management.service;
 
+
+import com.example.store.management.dto.CategoryDto;
+import com.example.store.management.entity.Category;
+import com.example.store.management.entity.Product;
+import com.example.store.management.mapper.CategoryMapper;
+import com.example.store.management.mapper.ProductMapper;
+import com.example.store.management.repository.CategoryRepository;
+import com.example.store.management.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+    private final CategoryMapper categoryMapper;
+    private final ProductMapper productMapper;
+
+    public CategoryDto saveCategory(CategoryDto categoryDto){
+        Category category=categoryMapper.convert(categoryDto);
+        //first saving parent
+        categoryRepository.save(category);
+
+        //saving product and first setting the category to it
+        for(Product product:category.getProduct()){
+            product.setCategory(category);
+            productRepository.save(product);
+        }
+        return categoryMapper.convert(category);
+    }
+
 }
